9e3baf97b4a62f9db1ef622a66e2b9bdbab6e2d1
AlanSottile <alan.sottile@heig-vd.ch>
2025-03-04 08:39:05 +0100

    Adding the solver

--- GRE_2425_labo1/src/main/java/ch/heig/gre/groupN/BfsSolver.java
+++ GRE_2425_labo1/src/main/java/ch/heig/gre/groupN/BfsSolver.java
@@ -6,9 +6,69 @@ import ch.heig.gre.maze.MazeSolver;
 
 import java.util.*;
 
+/**
+ * BfsSolver implements the BFS algorithm to find the shortest path between two vertices in a graph.
+ *
+ * @author Ripoll Pierric - Sottile Alan
+ */
+
 public final class BfsSolver implements MazeSolver {
-  @Override
-  public List<Integer> solve(Graph graph, int source, int destination, VertexLabelling<Integer> treatments) {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
+    @Override
+    public List<Integer> solve(Graph graph, int source, int destination, VertexLabelling<Integer> treatments) {
+        //number of vertices in the graph
+        int n = graph.nbVertices();
+
+        // 2 tables to store the distance to the source and predecessor of each vertex
+        int[] distance = new int[n];
+        int[] predecessor = new int[n];
+
+        // Initializing the BFS tables
+        for (int i = 0; i < n; i++) {
+            distance[i] = -1;
+            predecessor[i] = -1;
+        }
+
+        // The source has a distance of 0 from itself
+        distance[source] = 0;
+
+        // Initializing the queue
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+
+
+        // BFS algorithm
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            if (treatments != null) {
+                treatments.setLabel(current, distance[current]);
+            }
+
+            // if we are at the destination, there is no need to continue treatment
+            if (current == destination) {
+                break;
+            }
+
+            //for each unvisited neighbors of the current vertex
+            for (int neighbor : graph.neighbors(current)) {
+                if (distance[neighbor] == -1) {
+                    distance[neighbor] = distance[current] + 1;
+                    predecessor[neighbor] = current;
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        // Constructing reversed path (from dest to source)
+        List<Integer> path = new ArrayList<>();
+        for (int v = destination; v != -1; v = predecessor[v]) {
+            path.add(v);
+        }
+
+        //We need to reverse it because we filled it from the destination to the source
+        Collections.reverse(path);
+
+        return path;
+    }
 }
+
