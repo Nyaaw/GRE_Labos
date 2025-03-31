3cad1fdcfdfa063a3be44e4aa374a029ff1eaf37
Nyaaw <cactus318@hotmail.fr>
2025-03-31 17:52:42 +0200

    finished heuristics I think

--- GRE_2425_labo2/src/main/java/ch/heig/gre/groupN/AStar.java
+++ GRE_2425_labo2/src/main/java/ch/heig/gre/groupN/AStar.java
@@ -8,40 +8,59 @@ import ch.heig.gre.maze.GridMazeSolver;
 public final class AStar implements GridMazeSolver {
 
   public enum Heuristic {
+
     DIJKSTRA,
     
     INFINITY_NORM{
       @Override
-      public int computeHeuristic(int source, int destination, int k){
-        return 0;        
+      public double computeHeuristic(int source, int destination, int graphWidth, int kManhattan){
+        coords coord = new coords(source, destination, graphWidth);
+
+        return Math.max(Math.abs(coord.x1 - coord.x2), Math.abs(coord.y1 - coord.y2));
       }
     },
     
     EUCLIDEAN_NORM{
       @Override
-      public int computeHeuristic(int source, int destination, int k){
-        return 0;          
+      public double computeHeuristic(int source, int destination, int graphWidth, int kManhattan){
+        coords coord = new coords(source, destination, graphWidth);
+
+        return Math.sqrt(Math.pow(coord.x1 - coord.x2, 2) + Math.pow(coord.y1 - coord.y2, 2));
       }
     },
     
     MANHATTAN{
       @Override
-      public int computeHeuristic(int source, int destination, int k){
-        return 0;        
+      public double computeHeuristic(int source, int destination, int graphWidth, int kManhattan){
+        coords coord = new coords(source, destination, graphWidth);
+
+        return Math.abs(coord.x1 - coord.x2) + Math.abs(coord.y1 - coord.y2);        
       }
     },
     
     K_MANHATTAN{
       @Override
-      public int computeHeuristic(int source, int destination, int k){
-        return 0;        
+      public double computeHeuristic(int source, int destination, int graphWidth, int kManhattan){
+        coords coord = new coords(source, destination, graphWidth);
+
+        return kManhattan * Math.abs(coord.x1 - coord.x2) + Math.abs(coord.y1 - coord.y2);        
       }
     };
-
-    public int computeHeuristic(int source, int destination, int k){
-      return 0;
+    
+    private record coords(int x1, int x2, int y1, int y2){
+      public coords(int source, int destination, int graphWidth){
+        this(
+          source % graphWidth,
+          source / graphWidth,
+          destination % graphWidth,
+          destination / graphWidth
+        );
+      }
     }
 
+    public double computeHeuristic(int source, int destination, int graphWidth, int kManhattan){
+      return 0;
+    }
 
   }
 
